62c780afccb0e3ac54ff28d9d6174477ec96c1b9
Sam Knopper <samanthaknopper@hotmail.com>
2022-05-01 10:10:42 +0800

    base files created, html and notes pages linked by get requests

---
+++ server.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+const PORT = 3001;
+
+
+app.use( express.static('pibluc') );
+
+app.get('/', (req,res) => {
+
+    const indexHtmlPath = path.join(__dirname, 'public', 'index.html');
+    res.sendFile(indexHtmlPath);
+
+})
+
+app.get('/notes', (req,res) =>{
+    
+    const notesHtmlPath = path.join(__dirname, 'public', 'notes.html');
+    res.sendFile(notesHtmlPath);
+});
+
+
+app.listen(PORT, function(){
+    console.log(`App is running on http://localhost:${PORT}`)
+});
